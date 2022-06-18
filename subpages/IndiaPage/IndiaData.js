c19da2e04519175ef436a29d73b9a63bd360f757
saurav9709 <103635803+saurav9709@users.noreply.github.com>
2022-06-18 09:45:57 +0530

    indiaPage created

---
+++ subpages/IndiaPage/IndiaData.js
@@ -0,0 +1,47 @@
+
+let Data=[
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/jothimani_1200x768.jpeg?i49MTvtVEY76LmdtPUXJ_AlCPFzJ.QfL&size=170:96",title:"Congress MP Jothimani hospitalised, had claimed Delhi cops manhandled her amid protests",para: "Congress MP Jothimani was hospitalised on Saturday, mere days after alleging that she was “brutally assaulted” by Delhi Police personnel while protesting against party leader Rahul Gandhi’s questioning by the ED."},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/photogallery/202206/FVgEZzjaIAAKNpW_1200x768.jpeg?EggqVISSm0uy6eejmvHwHn2YufV2e097&size=170:96",title:"PM Modi meets mother Heeraben on her 100th birthday | In Pics",para: ""},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/WhatsApp_Image_2022-06-18_at_7_0_1200x768.jpeg?ssxjF3LKLx76psRZkJM819vd4.vkJD2y&size=170:96",title:"Ma isn’t a mere word: PM Modi writes as mother Heeraben Modi turns 100",para: "As his mother Heeraben Modi turns 100 today (June 18), Prime Minister Narendra Modi penned a heartfelt blog paying tribute to her and reminiscing on his life with her."},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/modi_mom_bday_4_1200x768.jpeg?1Rkt4V7qLUnEiJq_u6YzHrFjBj4rjxCU&size=170:96",title:"PM Modi visits mother to seek blessings as she turns 100 today",para: "Prime Minister Narendra Modi visited Gujarat's Gandhinagar to meet his mother Heeraben Modi and take her blessings as she turned 100 today. The Prime Minister washed her feet and sought her blessings."},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/collage-2_(6)_0.jpg?_al3w0cvThSWFbl9tuiT0hIdHBwgPJb4&size=170:96",title:"Protesters walk on Agnipath as Centre firefights: 1 killed, trains burnt, stations vandalised",para: "One person was killed in police firing, trains were torched, railway stations and highways turned into battlegrounds as youth fury over the Agnipath recruitment scheme continued to blaze across several states."},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/Agni_1200x768.png?Jw6eH52XzzjYYopF79eR6DEVpzocsZHE&size=170:96",title:"Protest against Agnipath: 15 held, 225 booked for violence, blocking expressway near Noida",para: "Noida’s Gautam Buddh Nagar Police lodged an FIR against 225 agitators and arrested 15 people, who, on Friday, vandalised vehicles and blocked the Yamuna Expressway as a sign of protest against the Centre’s Agnipath recruitment scheme."},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/breaking_news/202206/agnipath_protest_ANI_1200x768.jpeg?WDHmo1iCNwQKeriXYefjM6.NtV3P0Jxw&size=170:96",title:"10% quota for Agniveers in CAPFs, Assam Rifles; upper age limit raised by 5 yrs for 1st batch",para: ""},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/ASSAM_FLOOD_03_1200x768.jpeg?5VLUPFu8UTanO_.1QlLOkoJyZbBU2ciO&size=170:96",title:"Death toll rises to 17 as Assam flood situation worsens; 19 lakh people affected",para: "A total of 17 people, including two children, have died so far in the Assam floods, while one person has been reported missing. Nearly 19 lakh people in 2,930 villages across 12 districts have been affected."},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/EIIKU36SEJK45CLODNICDXIKLI_120.jpeg?dtq8qkoKKU10rVDg1PpHmhglfdBJU_Vo&size=170:96",title:"Jammu and Kashmir cop shot dead by terrorists at his Pulwama home",para: "Sub-Inspector Farooq Ahmad Mir was shot dead by terrorists inside his home in Pulwama district of Jammu and Kashmir, officials said on Saturday."},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/yo00_1200x768.jpeg?0ED8pLsePsid3LFIVcEDyAC35ruUXaPQ&size=170:96",title:"Agnipath protests: Army aspirant who lost life to police bullet came from a farming family",para: "Rakesh, an Army aspirant, who was allegedly killed in a police firing during protests against the Centre’s Agnipath recruitment scheme at Secunderabad railway station came from a family of farmers."},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/ramrahim_1200x768.jpeg?CdGJQjIbhxOuYT5rCqOL6wNnip2AVRJP&size=170:96",title:"Dera Chief Ram Rahim granted month-long parole; leaves Rohtak jail",para: "Gurmeet Ram Rahim Singh, who is currently serving a 20-year jail term for raping two women at his ashram, walked out of the Sunaria jail in Haryana's Rohtak on a month-long parole."},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/prezmeet_0_1200x768.jpeg?zLsCREIF8E3xg7YLZzILG1nwDrQ8putv&size=170:96",title:"Opposition meet on June 21 to finalise presidential poll candidate",para: "Opposition leaders from 17 parties are all set to meet on June 21 in Delhi to pick their presidential poll candidate."},
+  
+];
+
+
+// {avatar: "",title:"",para: ""},
+// {avatar: "",title:"",para: ""},
+
+
+
+
+
+
+let smallData1=[
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/Agniveer_military_graphic.jpg?size=88:50",title:"",para: "Agnipath: 11 countries where military service is compulsary | Interactive"},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/Capture_12_1200x768.png?size=88:50",title:"",para: "US midterm elections 2022: Biden's worries and key issues | Deep Dive"},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/Admiral_1200x768.png?size=88:50",title:"",para: "Move to modernise, transform armed forces: Chief of Naval Staff on Agnipath scheme"},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202206/rishabhpantindvsat20wc_1200x768.jpeg?size=88:50",title:"",para: "Is time running out for Rishabh Pant in T20Is?"},
+];
+
+let smallData2=[
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/video/202206/bihar_1200x768.png?size=88:50",title:"",para: "RJD calls for Bihar bandh tomorrow over Agnipath scheme"},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/video/202206/WhatsApp_Image_2022-06-17_at_4.jpeg?size=88:50",title:"",para: "Groom Kidnapping in Bihar: Veterinary Doctor Kidnapped, Forcefully Married in Begusarai"},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/video/202206/YT_&_FB_HORIZONTAL_copy_(24)_0.png?size=88:50",title:"",para: "House of cards actor Kevin Spacey given bail, what happens to #metoo now?"},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/video/202206/chole_1200x768.png?size=88:50",title:"",para: "Lizard Found In 'Chole Bhature' At Sagar Ratna In Chandigarh"},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/video/202206/proytest_0_1200x768.png?size=88:50",title:"",para: "Agnipath protest: Rs 3 lakh looted from ticket counter at Bihiya railway station in Bihar"},
+];
+
+let smallData3=[
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/video/202206/train_1-88x50.jpeg?vLaXLP4VJ0kW6qI1UqXxrf7TEdPkgnqu",title:"",para: "Indian Railways Launched First Private Train Under 'Bharat Gaurav Scheme"},
+    {avatar: "https://akm-img-a-in.tosshub.com/indiatoday/images/video/202206/bihar_0-88x50.png?8fYVrSokhC9z45WiYJb1gsOm0kPSG.nO",title:"",para: "Protests against Agnipath scheme rage on: Who's instigating aspirants and what's the way forward?"},
+    
+    
+];
